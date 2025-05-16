6de125b7e73b3834f47900fccb1404f260bc24d4
admin <904148597@qq.com>
2025-05-16 13:11:17 +0800

    add proxy cdn

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,25 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
 
-
+# evn dev live 
+ENV = "dev"
 class Settings(BaseSettings):
-    # API Configuration
-    SEGMENTATION_API_URL: str = "http://27.152.58.86:51055/api/segmentation/base64"
-    API_TIMEOUT: int = 60
+    if ENV == "dev":
+        # API Configuration
+        SEGMENTATION_API_URL: str = "http://27.152.58.86:51055/api/segmentation/base64"
+        API_TIMEOUT: int = 60
 
-    # File paths
-    OUTPUT_DIR: str = "output-images"
-    INPUT_DIR: str = "input-images"
+        # File paths
+        OUTPUT_DIR: str = "output-images"
+        INPUT_DIR: str = "input-images"
 
+    elif ENV == "live":
+        SEGMENTATION_API_URL: str = "http://localhost:51055/api/segmentation/base64"
+        API_TIMEOUT: int = 60
+
+        # File paths
+        OUTPUT_DIR: str = "x://sso/segment_images/output-images"
+        INPUT_DIR: str = "x://sso/segment_images/input-images"
     # Logging
     LOG_LEVEL: str = "INFO"
 
