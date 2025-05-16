bd408fd709003250cba5c605314357ba2a755176
admin <904148597@qq.com>
2025-05-16 13:08:36 +0800

    add proxy cdn

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 # main.py
 from fastapi import FastAPI
 from app.api.routes import router as image_router
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi import applications
+
+# 猴子补丁替换 CDN 链接
+def swagger_monkey_patch(*args, **kwargs):
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.6.2/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.6.2/swagger-ui.min.css"
+    )
+
+applications.get_swagger_ui_html = swagger_monkey_patch
 
 app = FastAPI()
 
